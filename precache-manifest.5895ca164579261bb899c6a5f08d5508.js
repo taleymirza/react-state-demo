ec07c5d928332dcfabc05456eab8fe5bd24022b3
Taley'a Mirza <taley.mirza@gmail.com>
2020-10-07 14:26:32 +0500

    Updates

---
+++ precache-manifest.5895ca164579261bb899c6a5f08d5508.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08049033b3b78c962a546147216c09b1",
+    "url": "/select-image/index.html"
+  },
+  {
+    "revision": "13f490175ce2a3670f73",
+    "url": "/select-image/static/css/main.a41397f9.chunk.css"
+  },
+  {
+    "revision": "b841792a974482228f12",
+    "url": "/select-image/static/js/2.2c608f68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/select-image/static/js/2.2c608f68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13f490175ce2a3670f73",
+    "url": "/select-image/static/js/main.e2a522ba.chunk.js"
+  },
+  {
+    "revision": "8ae6f85d25329eb0f3de",
+    "url": "/select-image/static/js/runtime-main.1b3ef545.js"
+  },
+  {
+    "revision": "5abcd79a0b22b04278c5a4ed5e30a8de",
+    "url": "/select-image/static/media/fruit.5abcd79a.png"
+  },
+  {
+    "revision": "bec2141cb374d194cfc9e236c0936d70",
+    "url": "/select-image/static/media/meat.bec2141c.png"
+  },
+  {
+    "revision": "5a8950499a84a4920c70f8fb6c874ce1",
+    "url": "/select-image/static/media/select.5a895049.jpg"
+  },
+  {
+    "revision": "df8b83dfe05c8befd8fcdc58a28bbaf9",
+    "url": "/select-image/static/media/veg.df8b83df.jpg"
+  }
+]);
