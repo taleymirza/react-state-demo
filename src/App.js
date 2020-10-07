5a73c4524ab1a850464692c2de754e5988bfb767
Taley'a Mirza <taley.mirza@gmail.com>
2020-10-07 14:18:12 +0500

    feat: add select components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
+import ImageContainer from './Components/ImageContainer/ImageContainer';
 
 function App() {
-  const [option, setOption] = React.useState("meat");
+  const [option, setOption] = React.useState("select");
   return (
     <div className="App">
-      <h3>Hello</h3>
+      <h1>Select Image</h1>
       <select value={option} onChange={(e) => setOption(e.target.value)}>
         <option value="select">Select</option>
         <option value="fruit">Fruit</option>
         <option value="veg">Vegetable</option>
         <option value="meat">Meat</option>
       </select>
+      <ImageContainer imageName={option} />
     </div>
   );
 }
---
+++ src/Components/ImageContainer/ImageContainer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import FruitImage from '../../static/Images/fruit.png';
+import VegImage from '../../static/Images/veg.jpg';
+import SelectImage from '../../static/Images/select.jpg';
+import MeatImage from '../../static/Images/meat.png';
+import './ImageContainer.css';
+
+
+const ImageContainer = ({ imageName }) => {
+
+    const getImage = (name) => {
+        switch (name) {
+            case "fruit":
+                return FruitImage;
+            case "veg":
+                return VegImage;
+            case "meat":
+                return MeatImage;
+            default:
+                return SelectImage;
+        }
+    }
+
+    return (
+        <div className="image-container">
+            <img src={getImage(imageName)} alt={imageName} />
+        </div>
+    )
+
+}
+
+export default ImageContainer
